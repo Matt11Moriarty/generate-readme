3bbc8992ebed42459f7e69da7dfad584af2fcb0d
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-08-15 18:21:31 -0700

    ended tutor session and cleaned up code to implement suggestions

--- index.js
+++ index.js
@@ -7,94 +7,96 @@ const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the title?",
-    "Describe your application.",
-    "What are the installation instructions?",
-    "What is the usage information?",
-    "What are the contribution guidelines?",
-    "What are the test instructions?",
-    "What license are you using?",
-    "Enter your Github username.",
-    "What is your email address?"
+    {
+        type: 'input',
+        message: "What is the title?",
+        name: 'title',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "Describe your application.",
+        name: 'description',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "What are the installation instructions?",
+        name: 'installation',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "What is the usage information?",
+        name: 'usage',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "What are the contribution guidelines?",
+        name: 'contribution',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "What are the test instructions?",
+        name: 'tests',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'list',
+        message: "What license are you using?",
+        name: 'license',
+        choices: [
+            {
+                name: "None",
+                value: null
+            },
+            {
+                name: "MIT",
+                value: {
+                    name: "MIT",
+                    url: "https://img.shields.io/badge/License-MIT-yellow.svg",
+                    badgeUrl: "https://opensource.org/licenses/MIT",
+                    textUrl: '[MIT](https://www.mit.edu/~amini/LICENSE.md)'
+                }
+            },
+            {
+                name: "Apache-2.0",
+                value: {
+                    name: "Apache-2.0",
+                    url: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+                    badgeUrl: "https://opensource.org/licenses/Apache-2.0",
+                    textUrl: "[Apache-2.0](https://www.apache.org/licenses/LICENSE-2.0)"
+                }
+            },
+            {
+                name: "Boost Software License 1.0",
+                value: {
+                    name: "Boost Software License 1.0",
+                    url: "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
+                    badgeUrl: "https://www.boost.org/LICENSE_1_0.txt",
+                    textUrl: '[Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)'
+                }
+            },
+        ],
+        validate: (value) => { return value ? true : 'Please select one of the options.' }
+    },
+    {
+        type: 'input',
+        message: "Enter your Github username.",
+        name: 'username',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    },
+    {
+        type: 'input',
+        message: "What is your email address?",
+        name: 'email',
+        validate: (value) => { return value ? true : 'I need a valid answer to continue' }
+    }
 ];
 
-inquirer.prompt(
-    [
-        {
-            type: 'input',
-            message: questions[0],
-            name: 'title',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        },
-       {
-            type: 'input',
-            message: questions[1],
-            name: 'description',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        }, 
-        {
-            type: 'input',
-            message: questions[2],
-            name: 'installation',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        }, 
-        {
-            type: 'input',
-            message: questions[3],
-            name: 'usage',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        }, 
-        {
-            type: 'input',
-            message: questions[4],
-            name: 'contribution',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        }, 
-        {
-            type: 'input',
-            message: questions[5],
-            name: 'tests',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        },
-        {
-            type: 'list',
-            message: questions[6],
-            name: 'license',
-            choices: [
-                "None",
-                "MIT",
-                "Apache-2.0",
-                "Boost Software License 1.0"
-            ],
-            validate: (value) => { return value ? true : 'Please select one of the options.' }
-        },
-        {
-            type: 'input',
-            message: questions[7],
-            name: 'username',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        },
-        {
-            type: 'input',
-            message: questions[8],
-            name: 'email',
-            validate: (value) => { return value ? true : 'I need a valid answer to continue' }
-        }
-    ]
-  )
-  .then((answers) => {
-    console.log(answers);
-    let markdownData = generateMarkdown.generateMarkdown(answers)
-    writeToFile('newReadMe.md', markdownData);
-  })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       console.log('Can\'t be rendered');
-//     } else {
-//       console.log('Something went wrong');
-//     }
-//   })
-  ;
+
 
 
 // TODO: Create a function to write README file
@@ -103,13 +105,28 @@ function writeToFile(fileName, data) {
     fs.writeFile(filePath, data, err => {
         if (err) throw err;
         console.log('Saved!');
-      }
-      )
+    }
+    )
 }
 
 // TODO: Create a function to initialize app
 function init() {
-
+    inquirer.prompt(
+        questions
+    )
+        .then((answers) => {
+            console.log(answers);
+            let markdownData = generateMarkdown(answers)
+            writeToFile('newReadMe.md', markdownData);
+        })
+        //   .catch((error) => {
+        //     if (error.isTtyError) {
+        //       console.log('Can\'t be rendered');
+        //     } else {
+        //       console.log('Something went wrong');
+        //     }
+        //   })
+        ;
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,56 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  return license ? `[![License](${license.url})](${license.badgeUrl})` : "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license == 'None' ? '' : '- [License](#license)';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return license ? `
+## License
+This application is covered by the ${license.name} license.
+\nMore infomation can be found here: ${license.textUrl}
+  ` : "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-
-  switch (data.license) {
-    case 'None':
-      licenseText = 'Not licensed'
-      licenseBadge = ''
-      break;
-    case 'MIT':
-      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-      licenseText = `This application is coverd by the ${data.license} license.
-      \nMore infomation can be found here: [MIT](https://www.mit.edu/~amini/LICENSE.md)`
-      break;
-    case 'Apache-2.0':
-      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-      licenseText = `This application is coverd by the ${data.license} license.
-      \nMore infomation can be found here: [Apache-2.0](https://www.apache.org/licenses/LICENSE-2.0)`
-      break;
-    case 'Boost Software License 1.0':
-      licenseBadge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
-      licenseText = `This application is coverd by the ${data.license} license.
-      \nMore infomation can be found here: [Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)`
-      break;
-    default:
-      licenseText = 'License not selected'
-      licenseBadge = ''
-  }
-
   return `
 # ${data.title}
 ## Description
 ${data.description}
 
-${licenseBadge}
+${renderLicenseBadge(data.license)}
   
 ## Table of Contents:
 
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
-- [License](#license)
+${renderLicenseLink(data.license)}
 
 ## Installation
 ${data.installation}
@@ -61,8 +46,7 @@ ${data.usage}
 ## Contribution
 ${data.contribution}
 
-## License
-${licenseText}
+${renderLicenseSection(data.license)}
 
 ## Tests
 ${data.tests}
@@ -75,6 +59,4 @@ If you have any questions, please feel free to email me at ${data.email}
 `;
 }
 
-module.exports = {
-  generateMarkdown
-};
+module.exports = generateMarkdown;
